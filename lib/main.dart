7d43a2fdc5947f671078228f8fd8edf989b8012d
Desmond Lightfoot <zolightfoot@gmail.com>
2022-04-24 20:48:57 -0400

    swipe feature to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:flirt/blocs/swipe/swipe_bloc.dart';
 import 'package:flirt/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'models/models.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +14,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (_) => SwipeBloc()..add(LoadUsersEvent(users: User.users)))
+      ],
+      child: MaterialApp(
+        title: 'Flirt',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flirt/blocs/swipe/swipe_bloc.dart';
 import 'package:flirt/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,46 +16,85 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Column(
-        children: [
-          UserCard(user: User.users[0]),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 8.0,
-              horizontal: 60,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ChoiceButton(
-                  width: 60,
-                  height: 60,
-                  hasGradient: false,
-                  size: 25,
-                  color: Theme.of(context).colorScheme.secondary,
-                  icon: Icons.clear_rounded,
+      body: BlocBuilder<SwipeBloc, SwipeState>(builder: (context, state) {
+        if (state is SwipeLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is SwipeLoaded) {
+          return Column(
+            children: [
+              Draggable(
+                child: UserCard(user: state.users[0]),
+                feedback: UserCard(user: state.users[0]),
+                childWhenDragging: UserCard(user: state.users[1]),
+                onDragEnd: (drag) {
+                  if (drag.velocity.pixelsPerSecond.dx < 0) {
+                    context.read<SwipeBloc>()
+                      ..add(SwipeLeftEvent(user: state.users[0]));
+                    print('Swipe left');
+                  } else {
+                    context.read<SwipeBloc>()
+                      ..add(SwipeRightEvent(user: state.users[0]));
+                    print('Swipe right');
+                  }
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 60,
                 ),
-                ChoiceButton(
-                  width: 60,
-                  height: 60,
-                  size: 30,
-                  hasGradient: true,
-                  color: Colors.white,
-                  icon: Icons.favorite,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        context.read<SwipeBloc>()
+                          ..add(SwipeLeftEvent(user: state.users[0]));
+                        print('Swipe left');
+                      },
+                      child: ChoiceButton(
+                        width: 60,
+                        height: 60,
+                        hasGradient: false,
+                        size: 25,
+                        color: Theme.of(context).colorScheme.secondary,
+                        icon: Icons.clear_rounded,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        context.read<SwipeBloc>()
+                          ..add(SwipeRightEvent(user: state.users[0]));
+                        print('Swipe right');
+                      },
+                      child: ChoiceButton(
+                        width: 60,
+                        height: 60,
+                        size: 30,
+                        hasGradient: true,
+                        color: Colors.white,
+                        icon: Icons.favorite,
+                      ),
+                    ),
+                    ChoiceButton(
+                      width: 60,
+                      height: 60,
+                      hasGradient: false,
+                      size: 25,
+                      color: Theme.of(context).primaryColor,
+                      icon: Icons.watch_later,
+                    ),
+                  ],
                 ),
-                ChoiceButton(
-                  width: 60,
-                  height: 60,
-                  hasGradient: false,
-                  size: 25,
-                  color: Theme.of(context).primaryColor,
-                  icon: Icons.watch_later,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+              ),
+            ],
+          );
+        } else {
+          return Text('Something went wrong');
+        }
+      }),
     );
   }
 }
